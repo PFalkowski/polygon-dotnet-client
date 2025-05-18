61b688cf0c0762430c2bd2d7769e16ea0bd28cf9
PFalkowski <piotr.falkowski.fm@gmail.com>
2025-05-18 20:30:48 +0200

    Bump NuGet version, add CONTRIBUTORS.md

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,3 @@
+Contributors
+
+-[PFalkowski](https://github.com/PFalkowski)
--- Polygon.Client/DependencyInjection/ServiceCollectionExtensions.cs
+++ Polygon.Client/DependencyInjection/ServiceCollectionExtensions.cs
@@ -24,6 +24,6 @@ namespace Polygon.Client.DependencyInjection
             });
 
             return services;
-        }
+        }   
     }
 }
